1e337cb2f184ae972d3ad49f927773a3535831e8
wuzhy <313388925@qq.com>
2015-12-23 20:35:16 +0800

    service.model

---
+++ manage/src/main/java/com/kinth/frame/manage/service/model/AuthoritySearch.java
@@ -0,0 +1,13 @@
+package com.kinth.frame.manage.service.model;
+
+public class AuthoritySearch {
+	private String name;
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+}
---
+++ manage/src/main/java/com/kinth/frame/manage/service/model/RoleSearch.java
@@ -0,0 +1,13 @@
+package com.kinth.frame.manage.service.model;
+
+public class RoleSearch {
+	private String name;
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+}
